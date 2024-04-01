173b0deb4878fd610faef7d26d4d596cdadc69e3
TanyaVerb <karra_t@mail.ru>
2024-04-01 15:23:45 +0300

    add constants

---
+++ index.js
@@ -0,0 +1,94 @@
+// window.setTimeout(() => {
+//   window.document.body.style.background = "green";
+// }, 1000);
+
+// console.log(document.documentElement);
+// console.log(document.documentElement.innerHTML);
+// console.log(document.documentElement.innerText);
+// console.log(document.documentElement.outerHTML);
+// console.log(document.documentElement.outerText);
+
+// console.log(document.documentElement.childNodes);
+// console.log(document.documentElement.firstChild);
+// console.log(document.documentElement.lastChild);
+// console.log(document.documentElement.lastChild.nextSibling);
+// console.log(document.documentElement.firstChild.nextSibling);
+// console.log(document.documentElement.lastChild.previousSibling);
+
+// console.log(document.documentElement.childNodes[0]);
+// console.log(document.documentElement.childNodes[1]);
+// console.log(document.documentElement.childNodes[2]);
+// console.log(document.documentElement.childNodes[3]);
+
+// console.log(document.documentElement.children);
+
+// const body = document.body;
+// console.log(body.parentElement);
+
+// _______________________________________________________
+// console.log(navigator.userAgent);
+// console.log(location.href);
+
+// const containerBtn = document.querySelector(".calc__btn-container");
+// console.log(containerBtn);
+// console.log(containerBtn.childNodes); //все дочерние узлы
+// console.log(containerBtn.childNodes[11]);
+// console.log(containerBtn.hasChildNodes()); //проверка наличия дочерних узлов
+// console.log(containerBtn.children); // все дочерние элементы (тэги)
+
+// const btns = document.querySelectorAll(".btn");
+// console.log(btns);
+// console.log(btns[4]);
+
+// for (const num of btns) {
+//   console.log(num);
+// }
+
+// console.dir(containerBtn); // список доступных свойств
+// ____________________________________________________________
+
+const cleanEntry = document.getElementById("clearCE");
+console.log(cleanEntry);
+
+const clear = document.getElementById("clearC");
+console.log(clear);
+
+const point = document.getElementById("point");
+console.log(point);
+const equal = document.querySelector(".btn_equal");
+console.log(equal);
+const divide = document.querySelector(".btn-divide");
+console.log(divide);
+const multiply = document.querySelector(".btn-multiply");
+console.log(multiply);
+const plus = document.querySelector(".btn-plus");
+console.log(plus);
+const minus = document.querySelector(".btn-minus");
+console.log(minus);
+
+const input = document.querySelector(".calc__screen");
+console.log(input);
+
+const num1 = document.querySelector(".btn-num-1");
+console.log(num1);
+const num2 = document.querySelector(".btn-num-2");
+console.log(num2);
+const num3 = document.querySelector(".btn-num-3");
+console.log(num3);
+const num4 = document.querySelector(".btn-num-4");
+console.log(num4);
+const num5 = document.querySelector(".btn-num-5");
+console.log(num5);
+const num6 = document.querySelector(".btn-num-6");
+console.log(num6);
+const num7 = document.querySelector(".btn-num-7");
+console.log(num7);
+const num8 = document.querySelector(".btn-num-8");
+console.log(num8);
+const num9 = document.querySelector(".btn-num-9");
+console.log(num9);
+const num0 = document.querySelector(".btn-num-0");
+console.log(num0);
+// console.log(num0.innerHTML);
+// console.log(num0.tagName);
+// console.log(num0.nodeName);
